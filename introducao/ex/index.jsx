7d4c0bffe16014da8ae9061ce45f5169e0a01416
David Carvalho <david.snt.carvalho@gmail.com>
2018-10-10 17:52:58 +0000

    Redux - Exemplo 2

--- introducao/ex/index.jsx
+++ introducao/ex/index.jsx
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import Field from './field'
-import fieldReducer from './fieldReducer'
+import Counter from './counter'
+import counterReducer from './counterReducer'
 
 const reducers = combineReducers({
-    // field: () => ({ value: 'Opa!!!' })
-    field: fieldReducer
+    counter: counterReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue='Teste' />
+        <Counter />
     </Provider>
     , document.getElementById('app')
 )
