3b8f5d5d991ea89b3ace4b5c9357af890668db95
David Carvalho <david.snt.carvalho@gmail.com>
2018-10-11 16:37:32 +0000

    Fix

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,14 +1,10 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
-const URL = 'http://localhost:3003/api/todos'
-
 export default props => (
-
     <div>
         <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
         <TodoForm />
