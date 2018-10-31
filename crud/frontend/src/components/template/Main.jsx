8516b8b015870443b6e6dec5b518af96167f142e
David Carvalho <david.snt.carvalho@gmail.com>
2018-10-31 14:54:24 -0300

    Templates

---
+++ crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import Header from './Header'
+import './Main.css'
+
+export default props => 
+    <Fragment>
+        <Header />
+        <main className="content">
+            Conteúdo
+        </main>
+    </Fragment>
