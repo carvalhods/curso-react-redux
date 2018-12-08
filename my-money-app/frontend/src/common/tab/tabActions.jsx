c3606c0d82fa914be3fbd63c207833d1e50dcdb1
David Carvalho <david.snt.carvalho@gmail.com>
2018-12-08 21:53:35 -0200

    Migrate to v16

---
+++ my-money-app/frontend/src/common/tab/tabActions.jsx
@@ -0,0 +1,18 @@
+import { reset as resetForm } from 'redux-form';
+import { init } from '../../billingCycle/billingCycleActions'
+
+export function selectTab(tabId) {
+    return {
+        type: 'TAB_SELECTED',
+        payload: tabId
+    }
+}
+
+export function showTabs(...tabIds) {
+    const tabsToShow = {}
+    tabIds.forEach(e => tabsToShow[e] = true)
+    return {
+        type: 'TAB_SHOWED',
+        payload: tabsToShow
+    }
+}
--- my-money-app/frontend/src/index.js
+++ my-money-app/frontend/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Routes from './main/routes'
+import reducers from './main/reducers'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>
+    , document.getElementById('app')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
